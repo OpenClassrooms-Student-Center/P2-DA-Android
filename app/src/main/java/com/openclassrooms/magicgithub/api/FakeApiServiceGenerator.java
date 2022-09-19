21c28091f874af1f9af5f3a1e53b1f8bea6402a2
Kirabium <virgile.fantauzzi@hotmail.fr>
2022-09-20 00:30:00 +0200

    Update to last gradle version and update image API

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
@@ -15,33 +15,33 @@ public abstract class FakeApiServiceGenerator {
     }
 
     public static List<User> FAKE_USERS = Arrays.asList(
-            new User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-            new User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-            new User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-            new User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-            new User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-            new User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-            new User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-            new User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-            new User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-            new User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-            new User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-            new User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-            new User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-            new User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-            new User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-            new User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-            new User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-            new User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-            new User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-            new User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+            new User("001", "Jake", "https://avatars.dicebear.com/api/miniavs/1.png"),
+            new User("002", "Paul", "https://avatars.dicebear.com/api/miniavs/2.png"),
+            new User("003", "Phil", "https://avatars.dicebear.com/api/miniavs/3.png"),
+            new User("004", "Guillaume", "https://avatars.dicebear.com/api/miniavs/4.png"),
+            new User("005", "Francis", "https://avatars.dicebear.com/api/miniavs/5.png"),
+            new User("006", "George", "https://avatars.dicebear.com/api/miniavs/6.png"),
+            new User("007", "Louis", "https://avatars.dicebear.com/api/miniavs/7.png"),
+            new User("008", "Mateo", "https://avatars.dicebear.com/api/miniavs/8.png"),
+            new User("009", "April", "https://avatars.dicebear.com/api/miniavs/9.png"),
+            new User("010", "Louise", "https://avatars.dicebear.com/api/miniavs/10.png"),
+            new User("011", "Elodie", "https://avatars.dicebear.com/api/miniavs/11.png"),
+            new User("012", "Helene", "https://avatars.dicebear.com/api/miniavs/12.png"),
+            new User("013", "Fanny", "https://avatars.dicebear.com/api/miniavs/13.png"),
+            new User("014", "Laura", "https://avatars.dicebear.com/api/miniavs/14.png"),
+            new User("015", "Gertrude", "https://avatars.dicebear.com/api/miniavs/15.png"),
+            new User("016", "Chloé", "https://avatars.dicebear.com/api/miniavs/16.png"),
+            new User("017", "April", "https://avatars.dicebear.com/api/miniavs/17.png"),
+            new User("018", "Marie", "https://avatars.dicebear.com/api/miniavs/18.png"),
+            new User("019", "Henri", "https://avatars.dicebear.com/api/miniavs/19.png"),
+            new User("020", "Rémi", "https://avatars.dicebear.com/api/miniavs/20.png")
     );
 
     public static List<User> FAKE_USERS_RANDOM = Arrays.asList(
-            new User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-            new User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-            new User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-            new User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-            new User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+            new User("021", "Lea", "https://avatars.dicebear.com/api/miniavs/21.png"),
+            new User("022", "Geoffrey", "https://avatars.dicebear.com/api/miniavs/22.png"),
+            new User("023", "Simon", "https://avatars.dicebear.com/api/miniavs/23.png"),
+            new User("024", "André", "https://avatars.dicebear.com/api/miniavs/24.png"),
+            new User("025", "Leopold", "https://avatars.dicebear.com/api/miniavs/25.png")
     );
 }
