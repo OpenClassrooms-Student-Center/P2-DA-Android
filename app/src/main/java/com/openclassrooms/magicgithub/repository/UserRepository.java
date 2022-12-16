2c4f65ee76d4ccd1850d5e3769fb2141a9fae07e
Sephigan <117346105+Sephigan@users.noreply.github.com>
2022-12-16 15:44:12 +0100

    Update UserRepository.java

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
